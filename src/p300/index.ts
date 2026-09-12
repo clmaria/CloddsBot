07c16b8fc2d3fcc010c9084fe29fa55dfbb49cfb
clmaria <mariajesusclagunas@gmail.com>
2026-09-13 00:32:23 +0200

    feat(p300): export Bitvavo read-only book synchronizer

--- src/p300/index.ts
+++ src/p300/index.ts
@@ -8,3 +8,4 @@ export * from './supervisor';
 export * from './profit-reserve';
 export * from './authorization-hash';
 export * from './anchored-reversion-evidence';
+export * from './bitvavo-book-sync';
